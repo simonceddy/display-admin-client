87b9b6b27c0de2ec5fddf826fb9f48a6c3a400ef
Simon Eddy <simon@simoneddy.com.au>
2022-07-11 15:21:51 +1000

    package updates, added video hook from public client

---
+++ src/hooks/useVideoMedia.js
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useVideoMedia() {
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [isFinished, setIsFinished] = useState(false);
+  const ref = useRef(null);
+
+  const play = () => {
+    if (!isPlaying && ref.current) ref.current.play();
+    setIsPlaying(true);
+  };
+  const pause = () => {
+    if (isPlaying && ref.current) ref.current.pause();
+    setIsPlaying(false);
+  };
+  const stop = () => {
+    if (ref.current) {
+      if (isPlaying) ref.current.pause();
+      ref.current.fastSeek(0);
+    }
+    setIsPlaying(false);
+  };
+
+  useEffect(() => {
+    if (ref) {
+      ref.current.on('ended', () => setIsFinished(true));
+    }
+  }, [ref]);
+
+  return {
+    isFinished,
+    isPlaying,
+    play,
+    pause,
+    ref,
+    stop
+  };
+}
+
+export default useVideoMedia;
